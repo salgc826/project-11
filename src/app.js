fe464f62db82d2cd9a70245110060267c98dc05e
salgc826 <csalgado307@gmail.com>
2018-03-10 22:57:09 -0800

    changes to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,51 @@
 'use strict';
 
 // load modules
-var express = require('express');
-var morgan = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const userRoutes = require('./routes/users');
+const courseRoutes = require('./routes/courses');
 
-var app = express();
+const User = require('./models/User').User;
+const Course = require('./models/Course').Course;
+const Review = require('./models/Review').Review;
+
+const app = express();
+
+// session to track logins
+app.use(session({
+  secret: 'this is a secret',
+  resave: true,
+  saveUninitialized: false
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var seeder = require('mongoose-seeder'),
+  data = require('./data/data.json');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+  console.error(`connection error: ${err}`);
+});
+
+db.once("open", function () {
+  seeder.seed(data).then(function (dbData) {
+    // The database objects are stored in dbData
+  }).catch(function (err) {
+    // handle error
+    console.error(err);
+  });
+  console.log(`db connection successful!`);
+});
 
 // set our port
 app.set('port', process.env.PORT || 5000);
@@ -12,26 +53,27 @@ app.set('port', process.env.PORT || 5000);
 // morgan gives us http request logging
 app.use(morgan('dev'));
 
-// setup our static route to serve files from the "public" folder
-app.use('/', express.static('public'));
+app.use('/api/users', userRoutes);
+app.use('/api/courses', courseRoutes);
 
 // catch 404 and forward to global error handler
-app.use(function(req, res, next) {
-  var err = new Error('File Not Found');
+app.use(function (req, res, next) {
+  let err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
 
 // Express's global error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
   res.status(err.status || 500);
-  res.render('error', {
+  res.send({
     message: err.message,
     error: {}
   });
 });
 
 // start listening on our port
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function () {
   console.log('Express server is listening on port ' + server.address().port);
 });
